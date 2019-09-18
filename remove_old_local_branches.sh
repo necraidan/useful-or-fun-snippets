fe87cf630074e8060ba1635cb9b67550a8dd76d7
Benjamin Auzanneau <benjamin.auzanneau.tm@gmail.com>
2019-09-18 09:24:31 +0200

    Update remove_old_local_branches.sh

--- remove_old_local_branches.sh
+++ remove_old_local_branches.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 git fetch
 git remote prune origin
+
 git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d
+or
+git branch --merged master | grep -v '^[ *]*master$' | xargs git branch -d
